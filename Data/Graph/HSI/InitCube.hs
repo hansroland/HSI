421beccb11328010fc29453e43e2911a0fdd8019
Roland Senn <rsx@bluewin.ch>
2022-12-08 20:41:17 +0100

    Fix bugs in InitCube

--- Data/Graph/HSI/InitCube.hs
+++ Data/Graph/HSI/InitCube.hs
@@ -1,37 +1,24 @@
-module Data.Graph.HSI.InitCube where
+module Data.Graph.HSI.InitCube (mkCube) where
 
 import Data.Graph.Dag
-import Data.Graph.HSI.Face
-import Data.Graph.HSI.Halfspace
-import Data.Graph.HSI.Polytope
+import Data.Graph.HSI.Face ( Visibility(Hidden), Face(..), Dim(..) )
+import Data.Graph.HSI.Halfspace ( HsKey, hsFromList, Halfspace )
+import Data.Graph.HSI.Polytope ( Polytope(..) )
 
 import Data.List ( elemIndices )
-
 import qualified Data.EnumMap as Map
 import qualified Data.Vector.Unboxed as VU
 
+-- --------------------------------------------------------------------
 -- Coefficients for a Halfspace equation for a cube
+-- --------------------------------------------------------------------
 data Coeff = Minus | Zero | Plus
     deriving (Eq, Show)
 
 -- Constants
-coValMax :: Double
+coValMax, coValMin:: Double
 coValMax =  10000
-coValMin :: Double
-coValMin = -1 * coValMax
-
--- value of a coefficient to calculate the NodeKey of a CFace
-coNodekeyVal :: Coeff -> NodeKey
-coNodekeyVal Zero  = 0
-coNodekeyVal Minus = 2
-coNodekeyVal Plus  = 1
-
--- value of a coefficient to calculate the HsKey of a CFace
--- (only for faces with dim -1 )
-coHskeyVal :: Coeff -> HsKey
-coHskeyVal Zero  = 0
-coHskeyVal Minus = 2
-coHskeyVal Plus  = 1
+coValMin = negate coValMax
 
 -- Value of a coefficient to calculate the coordinate of a cube vertex
 coCoordVal :: Coeff -> Double
@@ -39,41 +26,46 @@ coCoordVal Zero  =  0
 coCoordVal Minus = coValMax
 coCoordVal Plus  = coValMin
 
-
--- value of a coefficient to calculate the halfspace
+-- Value of a coefficient to calculate the halfspace
 coHsCoeff :: Coeff -> Double
 coHsCoeff Zero = 0
 coHsCoeff Minus = coValMin
 coHsCoeff Plus = coValMax
 
+-- --------------------------------------------------------------------
 -- A CubeFace (short CFace) is a list of Coeffs
+-- --------------------------------------------------------------------
 type CFace = [Coeff]
 
 -- Calculate the dimenstion of a CFace
 cfDim::  CFace -> Dim
 cfDim = Dim . length . filter (== Zero)
 
--- Generate all CFaces for a n-dimensional cube
-genAllCFaces :: Int -> [CFace]
-genAllCFaces  dim  = variateRep dim [Zero, Minus, Plus]
-
 -- Calculate the NodeKey of a CFace
 nodekeyVal :: CFace -> NodeKey
-nodekeyVal cFace =
-    let powers3 :: [NodeKey]
-        powers3 = [ 3^n | n <- [(0:: Int)..] ]
-    in  foldr (+) 0 $ zipWith (*) (coNodekeyVal <$> cFace)  powers3
+nodekeyVal cFace = foldr (+) 0 $ zipWith (*) (nkeyVal <$> cFace)  powers3
+  where
+    powers3 :: [NodeKey]
+    powers3 = [ 3^n | n <- [(0:: Int)..] ]
+     -- Value of a coefficient to calculate the NodeKey of a CFace
+    nkeyVal :: Coeff -> NodeKey
+    nkeyVal Zero  = 0
+    nkeyVal Minus = 2
+    nkeyVal Plus  = 1
 
 -- Calculate the HsKeys of a CFace
-hskeyVals :: CFace -> [HsKey]
-hskeyVals cf =
-    let poks = filter ((/= 0) . snd) $  zip [(0::HsKey)..] $ coHskeyVal <$> cf
-        hskey (a,b) = a * (2::HsKey) + b
-    in hskey <$> poks
+cfHskeyVals :: CFace -> [HsKey]
+cfHskeyVals cf = hskey <$> poks
+  where
+    poks = filter ((/= 0) . snd) $  zip [(0::HsKey)..] $ hkeyVal <$> cf
+    hskey (a,b) = a * (2::HsKey) + b
+    hkeyVal Zero  = 0
+    hkeyVal Minus = 2
+    hkeyVal Plus  = 1
 
--- create the sub-cFaces for a Zero at position n
--- to get the subfaces of a face we generate for every Zero 2 new
---    CFaces, where we replace the Zero by Plus and Minus
+-- Create the sub-CFaces for a Zero at position n
+-- to get the sub-CFaces of a CFace we generate for every Zero 2 new
+--    CFaces, where we replace the Zero by a Plus and a Minus
 subsAtN :: CFace -> Int -> [CFace]
 subsAtN cp n = [replCoeffAtN cp Plus n,  replCoeffAtN cp Minus n]
   where
@@ -86,64 +78,59 @@ subs es =
         cfaces = concat $ subsAtN es <$> ixs
     in  nodekeyVal <$> cfaces
 
--- make a face
-mkFace :: CFace -> Face
-mkFace cf =
-    let hsKeys = hskeyVals cf
-    in  if cfDim  cf== 0
-         then Vertex mempty (cfVector cf) hsKeys
-         else Nonvert mempty (cfDim cf) hsKeys Hidden
-
-mkNode :: CFace -> Node Face
-mkNode cf =
-    let face = mkFace cf
-        kids = subs cf
-    in Node {nodeKids = kids, nodeData = face}
-
-
-cfVector :: CFace -> VU.Vector Double
-cfVector cf = VU.fromList $ (coCoordVal <$> cf)
-
-cfHalfspace :: CFace -> Halfspace
-cfHalfspace cf = hsFromList $ (coHsCoeff <$> cf) ++  [coValMin]
-
-
-cubeFaces :: [CFace]
-cubeFaces = genAllCFaces 3                                  -- TODO n-dim
-
-hsmap :: Map.EnumMap HsKey Halfspace
-hsmap = Map.fromList $ zip keys hsvects
+-- --------------------------------------------------------------------
+-- Create a polytope of a given dimension
+-- --------------------------------------------------------------------
+mkCube :: Dim -> Polytope
+mkCube dim = Polytope { polyHs = hsmap, polyDag = dag}
   where
-    dim2s = filter (\cf -> cfDim cf == 2) cubeFaces         -- TODO n-dim
-    keys = (head . hskeyVals ) <$> dim2s
-    hsvects = cfHalfspace <$> dim2s
+    cubeFaces :: [CFace]
+    cubeFaces = genAllCFaces dim
 
-dag :: Dag Face
-dag =  Dag {start = 0, dagMap = dagmap }
-  where
-    dagmap = Map.fromList $ zip keys nodes
-    keys = nodekeyVal <$> cubeFaces
-    nodes = mkNode <$> cubeFaces
+    hsmap :: Map.EnumMap HsKey Halfspace
+    hsmap = Map.fromList $ zip keys hsvects
+      where
+        dim2s = filter (\cf -> cfDim cf == (dim - 1)) cubeFaces
+        keys = (head . cfHskeyVals ) <$> dim2s
+        hsvects = mkHalfspace <$> dim2s
 
+    dag :: Dag Face
+    dag =  Dag {start = 0, dagMap = dagmap }
+      where
+        dagmap = Map.fromList $ zip keys nodes
+        keys = nodekeyVal <$> cubeFaces
+        nodes = mkNode <$> cubeFaces
 
-cube3 :: Polytope
-cube3 = Polytope { polyHs = hsmap, polyDag = dag}
+    mkNode :: CFace -> Node Face
+    mkNode cf =
+        let face = mkFace cf
+            kids = subs cf
+        in Node {nodeKids = kids, nodeData = face}
+
+    mkFace :: CFace -> Face
+    mkFace cf =
+        let hsKeys = cfHskeyVals cf
+        in  if cfDim  cf== 0
+            then Vertex mempty (mkVector cf) hsKeys
+            else Nonvert mempty (cfDim cf) hsKeys Hidden
+
+    mkHalfspace :: CFace -> Halfspace
+    mkHalfspace cf = hsFromList $ (coHsCoeff <$> cf) ++  [coValMin]
+
+    mkVector :: CFace -> VU.Vector Double
+    mkVector cf = VU.fromList $ (coCoordVal <$> cf)
 
 -- -------------------------------------------------------------------
--- Support functions
+-- Generate all CFaces for a n-dimensional cube
 -- -------------------------------------------------------------------
-
--- See package `Combinatorics` from Hackage
-variateRep :: Int -> [a] -> [[a]]
-variateRep n x =
-   if n<0 then [] else iter n (\y -> concatMap (\z -> map (z:) y) x) [[]]
-
-{- |
-Compositional power of a function,
-i.e. apply the function @n@ times to a value.
-See: Simon Thompson: \"The Craft of Functional Programming\", page 172
--}
-{-# INLINE iter #-}
-iter :: Int -> (a -> a) -> a -> a
-iter 0 _ x = x
-iter n f x = f (iter (n-1) f x)
+genAllCFaces :: Dim -> [CFace]
+genAllCFaces  dim  = variateRep dim [Zero, Minus, Plus]
+  where
+    -- See package `Combinatorics` from Hackage
+    variateRep :: Dim -> [a] -> [[a]]
+    variateRep n x =
+        if n<0 then [] else iter n (\y -> concatMap (\z -> map (z:) y) x) [[]]
+-- Compositional power of a function, i.e. apply the function @n@ times to a value.
+    iter :: Dim -> (a -> a) -> a -> a
+    iter 0 _ x = x
+    iter n f x = f (iter (n-1) f x)
