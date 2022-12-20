853a1e87bfa21edaf7e12cf7cd534e7ae961baf5
Roland Senn <rsx@bluewin.ch>
2022-12-20 17:45:47 +0100

    Remove debugging stuff

--- app/Main.hs
+++ app/Main.hs
@@ -1,61 +1,11 @@
+-- A simple main program to calculate and draw polytopes
+-- from halfspaces
 module Main where
 
-import UI
-
-import Data.Graph.HSI
-import Control.Monad.State.Strict
+import UI ( uiLoop, uiInitState )
+import Control.Monad.State.Strict ( execStateT )
 
 main :: IO ()
 main = do
    _ <- execStateT uiLoop uiInitState
    return ()
-
-cut_base :: [Halfspace]
-cut_base = hsFromList <$> [[0,0,1, 50] ]
-cut_top :: [Halfspace]
-cut_top =  hsFromList <$> [[0,0,-1, -50] ]
-
-
-hslist_simple1 :: [Halfspace]
-hslist_simple1 = hsFromList <$> [
-    [0,0,1, -10],
-    [0,0,-1, -10],
-    [0,1,0, -10],
-    [0,-1,0, -10]
-    ]
-
-
-hslist_check :: [Halfspace]
-hslist_check =  hsFromList <$> [[0,0,1, -100] ]
-
-
-hslist_okta :: [Halfspace]
-hslist_okta = hsFromList <$> [
-  [ 1, 1, 1,   -1],
-  [-1, 1, 1,   -1],
-  [ 1,-1, 1,   -1],
-  [-1,-1, 1,   -1],
-  [ 1, 1,-1,   -1],
-  [ 1,-1,-1,   -1],
-  [-1, 1,-1,   -1],
-  [-1,-1,-1,   -1]
-  ]
-
-
-hslist_cube :: [Halfspace]
-hslist_cube = hsFromList <$> [
-    [ 1, 0, 0,   -1],
-    [-1, 0, 0,   -1],
-    [ 0, 1, 0,   -1],
-    [ 0,-1, 0,   -1],
-    [ 0, 0, 1,   -1],
-    [ 0, 0,-1,   -1]
-   ]
-
-hslist_tetra :: [Halfspace]
-hslist_tetra = hsFromList <$> [
-    [ 1,  1,  1,   -1],
-    [-1, -1,  1,   -1],
-    [-1,  1, -1,   -1],
-    [ 1, -1, -1,   -1]
-  ]
